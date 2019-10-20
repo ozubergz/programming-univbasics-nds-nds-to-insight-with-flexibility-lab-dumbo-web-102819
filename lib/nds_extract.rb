20ece2bec73805a287deb4c263b9a95f3ddab7f3
James Min <jmin499@gmail.com>
2019-10-20 20:18:08 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,48 +34,41 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  # GOAL: For each Hash in an Array (movies_collection), provide a collection
-  # of movies and a directors name to the movie_with_director_name method
-  # and accumulate the returned Array of movies into a new Array that's
-  # returned by this method.
-  #
-  # INPUT:
-  # * name: A director's name
-  # * movies_collection: An Array of Hashes where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Array of Hashes where each Hash represents a movie; however, they should all have a
-  # :director_name key. This addition can be done by using the provided
-  # movie_with_director_name method
+  aoh = []
+  movies_collection.map do |movie|
+    h = movie_with_director_name(name, movie)
+    aoh.push(h)
+  end
+  aoh
 end
 
 
 def gross_per_studio(collection)
-  # GOAL: Given an Array of Hashes where each Hash represents a movie,
-  # return a Hash that includes the total worldwide_gross of all the movies from
-  # each studio.
-  #
-  # INPUT:
-  # * collection: Array of Hashes where each Hash where each Hash represents a movie
-  #
-  # RETURN:
-  #
-  # Hash whose keys are the studio names and whose values are the sum
-  # total of all the worldwide_gross numbers for every movie in the input Hash
+  new_hash = {}
+  collection.map do |movie|
+    new_hash[movie[:studio]] = 0
+  end
+  
+  result = {}
+  new_hash.each do |studio, gross|
+    collection.map do |movie|
+      if(studio == movie[:studio])
+        gross += movie[:worldwide_gross]
+      end
+    end
+    result[studio] = gross
+  end
+  result
 end
 
 def movies_with_directors_set(source)
-  # GOAL: For each director, find their :movies Array and stick it in a new Array
-  #
-  # INPUT:
-  # * source: An Array of Hashes containing director information including
-  # :name and :movies
-  #
-  # RETURN:
-  #
-  # Array of Arrays containing all of a director's movies. Each movie will need
-  # to have a :director_name key added to it.
+  aoa = []
+  source.map do |elem|
+    name = elem[:name]
+    movies_coll = elem[:movies]
+    aoa.push(movies_with_director_key(name, movies_coll))
+  end
+  aoa
 end
 
 # ----------------    End of Your Code Region --------------------
